56f242da49072f7602ae7c8a1a212e287da78634
Gauthier POULET <gauthier.poulet@gmail.com>
2022-05-05 17:53:42 +0200

    update colors

--- app/src/main/java/com/example/pwnjetpackcompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/pwnjetpackcompose/ui/theme/Color.kt
@@ -6,3 +6,13 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Amber = Color(0xFFffe16b)
+val AmberContainer = Color(0xFF715c00)
+val Blue = Color(0xFF00658e)
+val BlueContainer = Color(0xFFc4e7ff)
+
+val DarkAmber = Color(0xFFebc300)
+val DarkAmberContainer = Color(0xFFffe16b)
+val DarkBlue = Color(0xFF7fcfff)
+val DarkBlueContainer = Color(0xFF00344c)
